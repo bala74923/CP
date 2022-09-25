cfeed9f8edea5631628334376d6b2cfa66b5853c
bala74923 <bala74923@gmail.com>
2022-09-25 22:18:03 +0530

    Template added

---
+++ Template/Main.java
@@ -0,0 +1,17 @@
+package Template;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc;
+    public static void main(String[] args) {
+        solve();
+    }
+    static void solve(){
+        sc = new Scanner(System.in);
+        int test = sc.nextInt();sc.nextLine();
+        while(test-->0){
+           // return solution method
+        }
+    }
+}
